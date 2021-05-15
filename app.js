3006e94ef3188e27b5313c411cea3fbb0d7b5a45
Ferenc Bujtas <anothercp3@gmail.com>
2021-05-15 09:20:56 +0200

    kocka

---
+++ app.js
@@ -0,0 +1,103 @@
+// DOM manipuláció, javascriptel módosítjuk a css-t és a html-t
+
+let scores, roundScore, activePlayer;
+
+function init() {
+  // a két játékos pontszáma egy két elemű tömbben lesz tárolva
+  // az első elem az ekső játékos pontszáma, a második a második játékos pontszáma
+  scores = [0, 0];
+
+  // az aktuális játékos kör alatt megszerzett pontjai
+  roundScore = 0;
+
+  // mindig az első játékos kezd
+  activePlayer = 0;
+
+  // beállítjuk a kezdő értékeket a UI-on is
+
+  document.querySelector('#score-0').textContent = 0;
+  document.querySelector('#score-1').textContent = 0;
+
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
+
+  // a játék kezdetekor a kockát eltüntetjük
+  // inline style-t adunk hozzá az img-hez...
+  document.querySelector('.dice').style.display = 'none';
+  // a gombokat megjelenítjük
+  document.querySelector('.btn-roll').style.display = 'block';
+  document.querySelector('.btn-hold').style.display = 'block';
+
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+init();
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// ha a roll dice gombra kattint a user
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  // console.log('rolling the dice...');
+  // 1. generálunk egy random számot 1 és 6 között
+  let dice = Math.floor(Math.random() * 6) + 1;
+  // console.log(dice);
+  // 2. Az eredményt megjelenítjük az UI-on
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  // string concatenation: szöveg összefűzés
+  diceDOM.setAttribute('src', 'dice-' + dice + '.png');
+  // Ha  a játékos 1-est dob, a roundScore értékét veszti, és a következő játékos jön
+  if (dice !== 1) {
+    // A dobott értéket kiszámoljuk, majd megjelenítjük a piros dobozban...
+    roundScore = roundScore + dice;
+
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    //  ha a játékos 1-est dobott
+  } else {
+    nextPlayer();
+  }
+});
+
+
+function nextPlayer() {
+  // roundScore értéket nullázuk a UI-on is
+  document.querySelector('#current-' + activePlayer).textContent = 0;
+  // a következő játékos jön
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  roundScore = 0;
+  //  toggle: ha rajta volt a class akkor leveszi, ha nem volt rajta, akkor rárakja
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+// ha a hold gombra rányom a játékos
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  // a játékos megszerzi a kör alatt szerzett pontjait
+  // az előző érték plusz a mostani
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  // update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  // ellenőrizzük, hogy van-e nyertes
+  if (scores[activePlayer] >= 20) {
+    // játék vége
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.btn-roll').style.display = 'none';
+    document.querySelector('.btn-hold').style.display = 'none';
+  } else {
+    nextPlayer();
+  }
+});
---
+++ basics.js
@@ -0,0 +1,36 @@
+// létrehozzuk a változót és értéket adunk neki
+// változó létrehozása: deklarálás
+// értékadás = value assignment
+let szam = 9;
+
+
+// value assignment
+// a bal oldalon lévő változóba bele töltődik a jobb oldalon levő érték
+szam = 20;
+
+// csak deklarálás
+let myNumber;
+
+// deklarálás három kulcsszóval lehet
+// nem használjuk
+var a;
+// let-et akkor használjuk, ha később a változó értékét módosítani akarjuk 
+let b;
+// const akkor használjuk, ha nem akarjuk módosítani a változó értékét
+const c = 10;
+
+let szoveg = 'hello';
+
+console.log('hello');
+
+
+// tömb: olyan típus, ami több másik típust tud tárolni
+const myArray = [3, 'hello', ['a', 'b']];
+
+console.log(myArray);
+
+// a tömbböt nullától indexeljük
+console.log(myArray[0]);
+
+// logikai érték (boolean), true vagy false lehet az értéke
+let kapcsolo = true;
